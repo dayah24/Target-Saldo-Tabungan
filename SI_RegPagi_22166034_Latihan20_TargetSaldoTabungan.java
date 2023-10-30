e29ceb2004801b834c3dd18a4ef4c5aa8942a52e
dayah24 <149245277+dayah24@users.noreply.github.com>
2023-10-30 09:45:53 +0700

    Add files via upload

---
+++ SI_RegPagi_22166034_Latihan20_TargetSaldoTabungan.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package si_regpagi_22166034_latihan20_targetsaldotabungan;
+import java.text.DecimalFormat;
+/**
+ *
+ * @author LENOVO
+ * Nama     : Nur Hidayah
+ * Kelas    : PBO 020
+ * NIM      : 22166034
+ * Deskripsi Program : Target Saldo Tabungan
+ */
+public class SI_RegPagi_22166034_Latihan20_TargetSaldoTabungan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here// Saldo Awal
+        double saldoAwal = 3500000;
+        // Bunga per Bulan (dalam persen)
+        double BungaPerBulan = 8.0;
+        // Lama (bulan)
+        double saldoTarget = 6000000;
+        
+         // Format saldo awal sebagai mata uang
+        DecimalFormat df = new DecimalFormat("#,###,###");
+        String saldoAwalFormatted = df.format(saldoAwal);
+
+        // Hitung saldo per bulan
+        int bulan = 0;
+        while (saldoAwal < saldoTarget) {
+            double bunga = saldoAwal * (BungaPerBulan / 100);
+            saldoAwal += bunga;
+            bulan++;
+        // Format saldo per bulan sebagai mata uang
+        String saldoBulanFormatted = df.format(saldoAwal);
+        System.out.println("Saldo di Bulan ke- " + bulan + ": Rp. " + saldoBulanFormatted);
+        }
+    }
+}
+   
